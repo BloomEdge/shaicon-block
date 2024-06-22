48c461c554ebd34f9c5801874287e34ded607ca0
shakibdshy <shakibdshy@gmail.com>
2024-06-22 21:57:42 +0600

    Update Line Duotone Icon

--- src/icons/solar-icon/index.js
+++ src/icons/solar-icon/index.js
@@ -5,3 +5,12 @@ export * from './styles/broken/notification-lines-remove-broken';
 export * from './styles/broken/notification-bell-broken';
 export * from './styles/broken/notification-bell-bing-broken';
 export * from './styles/broken/notification-bell-off-broken';
+
+// Line Duotone
+export * from './styles/line-duotone/notification-unread-line-duotone';
+export * from './styles/line-duotone/notification-unread-lines-line-duotone';
+export * from './styles/line-duotone/notification-remove-line-duotone';
+export * from './styles/line-duotone/notification-lines-remove-line-duotone';
+export * from './styles/line-duotone/notification-bell-line-duotone';
+export * from './styles/line-duotone/notification-bell-bing-line-duotone';
+export * from './styles/line-duotone/notification-bell-off-line-duotone';
