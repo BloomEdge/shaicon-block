6712fe458296534a3a8a32d4998bdf8a8e0cb19d
shakibdshy <shakibdshy@gmail.com>
2024-06-23 13:24:53 +0600

    Update Notification Outline Icon

--- src/icons/solar-icon/index.js
+++ src/icons/solar-icon/index.js
@@ -1,16 +1,25 @@
-export * from './styles/broken/notification-unread-broken';
-export * from './styles/broken/notification-unread-lines-broken';
-export * from './styles/broken/notification-remove-broken';
-export * from './styles/broken/notification-lines-remove-broken';
-export * from './styles/broken/notification-bell-broken';
-export * from './styles/broken/notification-bell-bing-broken';
-export * from './styles/broken/notification-bell-off-broken';
+export * from "./styles/broken/notification-unread-broken";
+export * from "./styles/broken/notification-unread-lines-broken";
+export * from "./styles/broken/notification-remove-broken";
+export * from "./styles/broken/notification-lines-remove-broken";
+export * from "./styles/broken/notification-bell-broken";
+export * from "./styles/broken/notification-bell-bing-broken";
+export * from "./styles/broken/notification-bell-off-broken";
 
 // Line Duotone
-export * from './styles/line-duotone/notification-unread-line-duotone';
-export * from './styles/line-duotone/notification-unread-lines-line-duotone';
-export * from './styles/line-duotone/notification-remove-line-duotone';
-export * from './styles/line-duotone/notification-lines-remove-line-duotone';
-export * from './styles/line-duotone/notification-bell-line-duotone';
-export * from './styles/line-duotone/notification-bell-bing-line-duotone';
-export * from './styles/line-duotone/notification-bell-off-line-duotone';
+export * from "./styles/line-duotone/notification-unread-line-duotone";
+export * from "./styles/line-duotone/notification-unread-lines-line-duotone";
+export * from "./styles/line-duotone/notification-remove-line-duotone";
+export * from "./styles/line-duotone/notification-lines-remove-line-duotone";
+export * from "./styles/line-duotone/notification-bell-line-duotone";
+export * from "./styles/line-duotone/notification-bell-bing-line-duotone";
+export * from "./styles/line-duotone/notification-bell-off-line-duotone";
+
+// Outline
+export * from "./styles/outline/notification-unread-outline";
+export * from "./styles/outline/notification-unread-lines-outline";
+export * from "./styles/outline/notification-remove-outline";
+export * from "./styles/outline/notification-lines-remove-outline";
+export * from "./styles/outline/notification-bell-outline";
+export * from "./styles/outline/notification-bell-bing-outline";
+export * from "./styles/outline/notification-bell-off-outline";
