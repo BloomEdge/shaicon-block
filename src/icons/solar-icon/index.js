bf95d937b276aedf90f2d6dc3b5f215e29447656
shakibdshy <shakibdshy@gmail.com>
2024-06-23 18:10:04 +0600

    Update File Name

--- src/icons/solar-icon/index.js
+++ src/icons/solar-icon/index.js
@@ -1,34 +1,34 @@
-export * from "./styles/broken/notification-unread-broken";
-export * from "./styles/broken/notification-unread-lines-broken";
-export * from "./styles/broken/notification-remove-broken";
-export * from "./styles/broken/notification-lines-remove-broken";
-export * from "./styles/broken/notification-bell-broken";
-export * from "./styles/broken/notification-bell-bing-broken";
-export * from "./styles/broken/notification-bell-off-broken";
+export * from "./notification/broken/unread";
+export * from "./notification/broken/unread-lines/";
+export * from "./notification/broken/remove";
+export * from "./notification/broken/lines-remove";
+export * from "./notification/broken/bell";
+export * from "./notification/broken/bell-bing";
+export * from "./notification/broken/bell-off";
 
 // Line Duotone
-export * from "./styles/line-duotone/notification-unread-line-duotone";
-export * from "./styles/line-duotone/notification-unread-lines-line-duotone";
-export * from "./styles/line-duotone/notification-remove-line-duotone";
-export * from "./styles/line-duotone/notification-lines-remove-line-duotone";
-export * from "./styles/line-duotone/notification-bell-line-duotone";
-export * from "./styles/line-duotone/notification-bell-bing-line-duotone";
-export * from "./styles/line-duotone/notification-bell-off-line-duotone";
+export * from "./notification/line-duotone/unread";
+export * from "./notification/line-duotone/unread-lines";
+export * from "./notification/line-duotone/remove";
+export * from "./notification/line-duotone/lines-remove";
+export * from "./notification/line-duotone/bell";
+export * from "./notification/line-duotone/bell-bing";
+export * from "./notification/line-duotone/bell-off";
 
 // Outline
-export * from "./styles/outline/notification-unread-outline";
-export * from "./styles/outline/notification-unread-lines-outline";
-export * from "./styles/outline/notification-remove-outline";
-export * from "./styles/outline/notification-lines-remove-outline";
-export * from "./styles/outline/notification-bell-outline";
-export * from "./styles/outline/notification-bell-bing-outline";
-export * from "./styles/outline/notification-bell-off-outline";
+export * from "./notification/outline/unread";
+export * from "./notification/outline/unread-lines";
+export * from "./notification/outline/remove";
+export * from "./notification/outline/lines-remove";
+export * from "./notification/outline/bell";
+export * from "./notification/outline/bell-bing";
+export * from "./notification/outline/bell-off";
 
 // Bold
-export * from "./styles/bold/notification-unread-bold";
-export * from "./styles/bold/notification-unread-lines-bold";
-export * from "./styles/bold/notification-remove-bold";
-export * from "./styles/bold/notification-lines-remove-bold";
-export * from "./styles/bold/notification-bell-bold";
-export * from "./styles/bold/notification-bell-bing-bold";
-export * from "./styles/bold/notification-bell-off-bold";
+export * from "./notification/bold/unread";
+export * from "./notification/bold/unread-lines";
+export * from "./notification/bold/remove";
+export * from "./notification/bold/lines-remove";
+export * from "./notification/bold/bell";
+export * from "./notification/bold/bell-bing";
+export * from "./notification/bold/bell-off";
